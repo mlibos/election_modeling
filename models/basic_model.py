e88a84aa27b118432c539970b19b3218995c02a6
mlibos <munir.libos@gmail.com>
2020-11-16 12:50:02 -0600

    Update basic_model.py

--- models/basic_model.py
+++ models/basic_model.py
@@ -1,4 +1,5 @@
 import os
+import csv
 from pathlib import Path 
 from zipfile import ZipFile 
 
@@ -20,17 +21,29 @@ states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA",
           "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", 
           "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
 
+
+csvfile = csv.reader(file)
 # for now I will try to compute a polling average with error for a single state to test my skills.
 def csv_reader(csv):
 	#takes a poll in csv format and attempts to read it and collect data
 	#csv is in this format: state,pollster,sponsor,start.date,end.date,entry.date.time..et.,number.of.observations,population,mode,biden,trump,biden_margin,other,undecided,url,include,notes
 	state = 'MA'
+	polls = []
 	for poll in csv:
-		print(poll)
-		break
-	for poll in csv:
-		if poll[0:2] == state:
-			print(poll)
+		if poll[0] == state:
+			polls.append(poll)
+	biden_mean = 0
+	trump_mean = 0
+	totalsample = 0
+	for poll in polls:
+		totalsample += int(poll[6])
+		biden_mean += int(poll[6])*int(poll[9])
+		trump_mean += int(poll[6])*int(poll[10])
+		
+	biden_mean = round(biden_mean/totalsample,1)
+	trump_mean = round(trump_mean/totalsample,1)
+	print(biden_mean,trump_mean,totalsample)
 
 
-csv_reader(file)
+
+csv_reader(csvfile)
