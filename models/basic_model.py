4b217d887b244bc8cdea7c01fd9298625aefefdd
mlibos <munir.libos@gmail.com>
2020-11-16 12:22:32 -0600

    Update basic_model.py

--- models/basic_model.py
+++ models/basic_model.py
@@ -14,22 +14,23 @@ for file in data_files:
 	if '2020 US' in file:
 		poll = file
 file = open(poll, 'r')
-states = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado",
-  "Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois",
-  "Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland",
-  "Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana",
-  "Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York",
-  "North Carolina","North Dakota","Ohio","Oklahoma","Oregon","Pennsylvania",
-  "Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah",
-  "Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"]
+states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA", 
+          "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", 
+          "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", 
+          "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", 
+          "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+
+# for now I will try to compute a polling average with error for a single state to test my skills.
 def csv_reader(csv):
 	#takes a poll in csv format and attempts to read it and collect data
 	#csv is in this format: state,pollster,sponsor,start.date,end.date,entry.date.time..et.,number.of.observations,population,mode,biden,trump,biden_margin,other,undecided,url,include,notes
-	num = 0
+	state = 'MA'
 	for poll in csv:
-		num +=1
-		if num >10:
-			break
+		print(poll)
+		break
+	for poll in csv:
+		if poll[0:2] == state:
+			print(poll)
 
 
 csv_reader(file)
