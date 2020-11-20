57c3cc7668be01e47273043c582974d8b1105791
mlibos <munir.libos@gmail.com>
2020-11-20 01:03:34 -0600

    Update basic_model.py
    completed major work on creating vote intervals for every state in csvpoll2020

--- models/basic_model.py
+++ models/basic_model.py
@@ -10,33 +10,22 @@ for entry in entries.iterdir():
 	file = 'C:/Users/SEDan/Documents/election_modeling/data/' + entry.name
 	data_files.append(file)
 
-poll = ''
-for file in data_files:
-	if '2020 US' in file:
-		poll = file
-file = open(poll, 'r')
-states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA", 
-          "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", 
-          "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", 
-          "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", 
-          "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
 
 
-csvfile = csv.reader(file)
-# for now I will try to compute a polling average with error for a single state to test my skills.
-def csv_reader(csv):
-	#takes a poll in csv format and attempts to read it and collect data
-	#csv is in this format: state,pollster,sponsor,start.date,end.date,entry.date.time..et.,number.of.observations,population,mode,biden,trump,biden_margin,other,undecided,url,include,notes
-	state = 'MA'
+def find_polls(csvfile,state):
+	#finds polls pertaining to given state
 	polls = []
-	for poll in csv:
+	for poll in csvfile:
 		if poll[0] == state:
 			polls.append(poll)
+	return (polls)
+
+def interval_calc(polls):
+	#takes a list of polls and computes a 95% confidence interval of vote shares
 	biden_mean = []
 	trump_mean = []
 	totalsample = [0,[]]
-
-	#equation for combined_std = sqrt((txx-tx2/tn) / (tn-1)) where tn = total n tx = sum of mean*n txx = sum of tx^2
+	#taking poll results from our poll list and creating lists containing vote totals and sample sizes
 	for poll in polls:
 		n = int(poll[6])
 		biden = int(poll[9])/100
@@ -45,22 +34,55 @@ def csv_reader(csv):
 		totalsample[1].append(n)
 		biden_mean.append(biden)
 		trump_mean.append(trump)
-	biden_tx = 0
-	biden_txx = 0
+	#calculating weighted means using a pooled sample
+	biden_weighted_mean = 0
+	trump_weighted_mean = 0
 	for index,mean in enumerate(biden_mean):
-		biden_tx += mean*totalsample[1][index]
-		biden_txx += (mean*totalsample[1][index])**2
-	trump_tx = 0
-	trump_txx = 0
+		biden_weighted_mean += biden_mean[index]*totalsample[1][index]
 	for index,mean in enumerate(trump_mean):
-		trump_tx += mean*totalsample[1][index]
-		trump_txx += (mean*totalsample[1][index])**2
-	biden_std = ((biden_txx-biden_tx**2/totalsample[0])/(totalsample[0]-1))**0.5
-	trump_std = ((trump_txx-trump_tx**2/totalsample[0])/(totalsample[0]-1))**0.5	
+		trump_weighted_mean += trump_mean[index]*totalsample[1][index]
+	biden_weighted_mean = round(biden_weighted_mean/totalsample[0],3)
+	trump_weighted_mean = round(trump_weighted_mean/totalsample[0],3)
+	#calculating variance and std_dev
+	biden_variance = round(totalsample[0]*biden_weighted_mean*(1-biden_weighted_mean),2)
+	trump_variance = round(totalsample[0]*trump_weighted_mean*(1-trump_weighted_mean),2)
+	biden_std_dev = round(biden_variance**0.5,2)
+	trump_std_dev = round(trump_variance**0.5,2)
+	#taking std_dev and calculating a 95% confidence interval
+	biden_interval = [biden_weighted_mean*totalsample[0]-biden_std_dev*1.92,biden_weighted_mean*totalsample[0]+biden_std_dev*1.92]
+	trump_interval = [trump_weighted_mean*totalsample[0]-trump_std_dev*1.92,trump_weighted_mean*totalsample[0]+trump_std_dev*1.92]
+	for i in range(2):
+		biden_interval[i] = round(biden_interval[i]/totalsample[0],3)
+		trump_interval[i] = round(trump_interval[i]/totalsample[0],3)
+	biden_interval.insert(1,biden_weighted_mean)
+	trump_interval.insert(1,trump_weighted_mean)
+	intervals = [biden_interval,trump_interval]
+	return intervals
 
-	print(biden_mean,trump_mean,totalsample)
-	print(biden_std,trump_std)
+def csv_reader(filename):
+	#takes a poll in csv format and attempts to read it and collect data
+	#csv is in this format: state,pollster,sponsor,start.date,end.date,entry.date.time..et.,number.of.observations,population,mode,biden,trump,biden_margin,other,undecided,url,include,notes
+	states = ["--","AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+	state_intervals = {}
+	for state in states:
+		#convert to reader csv
+		file = open(filename, 'r')
+		csvfile = csv.reader(file)
+		#find polls for state
+		polls = find_polls(csvfile,state)
+		#compute intervals
+		if polls:
+			state_intervals[state] = interval_calc(polls)
+		else:
+			state_intervals[state] = None
+	state_intervals['NATIONAL'] = state_intervals['--']
+	del state_intervals['--']
+	return state_intervals
+for file in data_files:
+	if '2020 US' in file:
+		filename = file
 
 
-
-csv_reader(csvfile)
+state_intervals = csv_reader(filename)
+for state in state_intervals:
+	print(state,state_intervals[state])
